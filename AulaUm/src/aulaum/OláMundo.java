909e8f0174d2e836ee4fe6e5b9ca937b9b6d9ba0
User <User@DESKTOP-G55DH5D>
2023-11-04 12:58:32 -0300

    Olámundo e Teclado

---
+++ AulaUm/src/aulaum/OláMundo.java
@@ -0,0 +1,14 @@
+package aulaum;
+
+import java.util.Scanner;
+
+public class OláMundo {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner (System.in);
+	System.out.print ("digite seu nome");
+	String nome = scanner.nextLine();
+
+	
+
+}
+}
